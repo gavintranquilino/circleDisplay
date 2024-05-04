ce46a90c79a3dfb7a22f361366c7ba3197e5ef31
Gavin T <gavintranquilino@gmail.com>
2024-05-04 08:56:37 -0400

    Refactor

--- main/main.c
+++ main/main.c
@@ -10,16 +10,16 @@
 
 #define STACK_SIZE 2048
 
-void LCD(void * arg)
+void LCD(void *arg)
 {
     uint16_t Color;
     GC9A01_Init();
-    for(;;)
+    for (;;)
     {
-        Color=rand();
-        GC9A01_FillRect(0,0,239,239,Color);
+        Color = rand();
+        GC9A01_FillRect(0, 0, 239, 239, Color);
         GC9A01_Update();
-        vTaskDelay(1000/portTICK_PERIOD_MS);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
 
@@ -27,6 +27,6 @@ void app_main(void)
 {
     TaskHandle_t LCDHandle;
 
-    xTaskCreate(LCD,"Test LCD",STACK_SIZE,NULL,tskIDLE_PRIORITY,&LCDHandle);
+    xTaskCreate(LCD, "Test LCD", STACK_SIZE, NULL, tskIDLE_PRIORITY, &LCDHandle);
     configASSERT(LCDHandle);
 }
